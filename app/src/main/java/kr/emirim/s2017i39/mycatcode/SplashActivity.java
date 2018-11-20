64077a59630effea2db3be1230f9fbb628d418db
Mirim Ju <wnalfla@gmail.com>
2018-11-20 20:49:18 +0900

    주석

--- app/src/main/java/kr/emirim/s2017i39/mycatcode/SplashActivity.java
+++ app/src/main/java/kr/emirim/s2017i39/mycatcode/SplashActivity.java
@@ -2,6 +2,7 @@ package kr.emirim.s2017i39.mycatcode;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Log.d("스플래시액티비티","onCreate 실행");
     }
 }
